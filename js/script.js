b55ed2802e1fe45fd34761cc3118c8f6a475af68
Edoardo Pizzicannella <edoardo.pizzicannella@gmail.com>
2021-01-07 15:52:00 +0100

    iniziato secondo esercizio

--- js/script.js
+++ js/script.js
@@ -14,3 +14,27 @@ if (reverseString(userWord) === userWord) {
 } else {
   alert('la parola che hai digitato non è Palindroma');
 }
+
+
+//ESERCIZIO 2
+//utilizzare un flag per controllare se corrisponde alla scelta dell'utente
+//creo gl'input che utilizzerò nell'esercizio
+
+var oddOrEven = prompt ('pari o dispari?');
+console.log('la scelta dell\'utente è stata ' + oddOrEven);
+var userNum = parseInt(prompt('scegli un numero da 1 a 5'));
+console.log('il numnero scelto dall\'utente è stato' + userNum;
+
+
+// creo funzione per generare un numero random
+function randomNumber(min, max) {
+  var result = Math.floor(Math.random() * (max + 1 - min) + min);
+  return result;
+}
+
+// creo la variabile con il numero del computer
+var computerNum = randomNumber(1, 5);
+console.log('il numero del computer è ' + computerNum);
+
+var total = userNum + computerNum
+console.log(total);
